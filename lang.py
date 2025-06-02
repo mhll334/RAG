7751e26c4186f6fb4dfb6817f62d0135db9431c5
serena9312 <serena.tasiwen@gmail.com>
2025-06-03 00:55:34 +0800

    update lang.py

--- lang.py
+++ lang.py
@@ -1,9 +1,10 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_nomic.embeddings import NomicEmbeddings
+from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_ollama import ChatOllama
 from langchain.chains import RetrievalQA
+import os
 
 loader = TextLoader("C:/Users/Serena Li/OneDrive/Desktop/實驗室/team/【愛健康│理財生活通】陳亮恭醫師談「在家養老做得到嗎？」.txt", encoding = "utf-8")
 documents = loader.load()
@@ -14,7 +15,7 @@ doc_splits = text_splitter.split_documents(documents)
 vectorstore = Chroma.from_documents(
     documents = doc_splits,
     collection_name = "local-rag",
-    embedding = NomicEmbeddings(model = "nomic-embed-text-v1.5", inference_mode = "local"),
+    embedding = OllamaEmbeddings(model = "mxbai-embed-large"),
 )
 retriever = vectorstore.as_retriever()
 
@@ -26,7 +27,8 @@ rag_chain = RetrievalQA.from_chain_type(
     return_source_documents = True,
 )
 
-query = "請說明在家養老需要做什麼準備?"
+query = input("Question: ")
+
 result = rag_chain.invoke({"query": query}) 
 
 print("Answer: ", result["result"])
